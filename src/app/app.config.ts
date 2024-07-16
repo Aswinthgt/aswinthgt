3c591c936ad3da639a95144312e3ac25c0f723ba
aswinth <aswinth37@outlook.com>
2024-07-16 09:09:32 +0530

    Speedlight crash fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,6 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { injectSpeedInsights } from '@vercel/speed-insights';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +12,5 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideAnimationsAsync(),
     provideHttpClient(withFetch()),
-    // { provide: APP_INITIALIZER, useFactory: injectSpeedInsights },
   ],
 };
