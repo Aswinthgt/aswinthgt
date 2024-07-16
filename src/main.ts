a7cfe7bf14caf5792ce31fdd6cc9f016bb8d6c1f
aswinth <aswinth37@outlook.com>
2024-07-16 09:15:20 +0530

    analytics added

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject } from '@vercel/analytics';
 
 injectSpeedInsights();
+inject();
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
