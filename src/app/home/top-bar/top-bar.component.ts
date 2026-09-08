78f7b871b09e79ec18e3762dae6f08889fab8517
Aswinth <aswinth37@outlook.com>
2026-09-08 22:37:20 +0530

    feat: implement smooth View Transition theme switching with a custom palette drawer UI

--- src/app/home/top-bar/top-bar.component.ts
+++ src/app/home/top-bar/top-bar.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, inject, HostListener, HostBinding } from '@angular/core';
+import { Component, OnInit, inject, HostListener, HostBinding, ElementRef } from '@angular/core';
 import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
 
 import { CommonService } from '../shared/commonService/common.service';
 import { Navigate, ThemePallete } from '../../models/models';
@@ -10,17 +9,38 @@ import { gallery } from '../shared/static';
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [MatRippleModule, MatIconModule, MatMenuModule],
+  imports: [MatRippleModule, MatIconModule],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss'
 })
 export class TopBarComponent implements OnInit {
 
-  commonService = inject(CommonService)
-  colorGalaries = gallery
+  commonService = inject(CommonService);
+  elementRef = inject(ElementRef);
+  colorGalaries = gallery;
+
+  isPaletteOpen = false;
   
   @HostBinding('class.navbar-hidden') isHidden = false;
   lastScrollTop = 0;
+
+  togglePaletteDrawer(event: MouseEvent) {
+    event.stopPropagation();
+    this.isPaletteOpen = !this.isPaletteOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (this.isPaletteOpen && !this.elementRef.nativeElement.contains(target)) {
+      this.isPaletteOpen = false;
+    }
+  }
+
+  @HostListener('window:keydown.escape')
+  onEscape() {
+    this.isPaletteOpen = false;
+  }
 
 
   @HostListener('window:scroll')
@@ -46,14 +66,16 @@ export class TopBarComponent implements OnInit {
     this.commonService.scrollTo(menu)
   }
 
-  setColorPallete(pallete: ThemePallete) {
-    this.commonService.currentTheme = pallete
-    this.commonService.setTheme();
+  setColorPallete(pallete: ThemePallete, event?: MouseEvent) {
+    this.commonService.setThemeWithTransition(() => {
+      this.commonService.currentTheme = pallete;
+    }, event);
   }
 
-  changeMode() {
-    this.commonService.isDarkMode = !this.commonService.isDarkMode
-    this.commonService.setTheme();
+  changeMode(event?: MouseEvent) {
+    this.commonService.setThemeWithTransition(() => {
+      this.commonService.isDarkMode = !this.commonService.isDarkMode;
+    }, event);
   }
 
 }
